e919c9cf8a927bc680f7feb86b86ed255b060509
tan <115695685+hoangtan2002@users.noreply.github.com>
2022-11-05 04:31:41 -0700

    fix bugs

--- Core/Src/fsm_manual.c
+++ Core/Src/fsm_manual.c
@@ -10,16 +10,16 @@ void fsmState1(){
 	if(isButton2Pressed() == 1 && isButton2LongPressed() == 0){
 		ledBuffer++;
 		if(ledBuffer == 10) ledBuffer = 1;
-		timeOut = 10;
+		setTimer2(1000);
 	}
 	if(isButton3Pressed() == 1 && isButton2LongPressed() == 0){
 		ledBuffer--;
 		if(ledBuffer == 0) ledBuffer = 9;
-		timeOut = 10;
+		setTimer2(1000);
 	}
 	if(isButton1Pressed() == 1){
 		ledBuffer = 1;
-		timeOut = 10;
+		setTimer2(1000);
 	}
 }
 
@@ -27,29 +27,18 @@ void fsmState2(){
 	if(isButton2LongPressed() == 1 && isButton2Pressed() == 0){
 		ledBuffer++;
 		if(ledBuffer == 10) ledBuffer = 1;
-		timeOut = 10;
+		setTimer2(1000);
 	}
-	if(isButton3LongPressed() == 1 && isButton2Pressed() == 0){
+	if(isButton3LongPressed() == 1 && isButton3Pressed() == 0){
 		ledBuffer--;
 		if(ledBuffer == 0) ledBuffer = 9;
-		timeOut = 10;
+		setTimer2(1000);
 	}
 }
 
-void countTimeOut(){
-	if(timeOut == 10){
-		isTimedOut = 0;
-	}
-	if(timeOut == 0){
-		isTimedOut = 1;
-	}
-	if(timer2_flag==1){
-		timeOut--;
-		setTimer2(100);
-	}
-}
+
 void fsmState3(){
-	if(isTimedOut == 0){
+	if(timer2_flag==0){
 		return;
 	}
 	if(timer3_flag==1){
